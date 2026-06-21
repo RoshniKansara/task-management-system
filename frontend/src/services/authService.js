50f4aa4edce34463235496563756e26a933648b8
Roshni Kansara <roshnikansara0510@gmail.com>
2026-06-21 18:34:57 +0530

    Fix remaining localhost URLs

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/auth";
+const API_URL = "https://task-management-backend-3ecl.onrender.com/auth";
 
 export const loginUser = async (email, password) => {
   const response = await axios.post(
