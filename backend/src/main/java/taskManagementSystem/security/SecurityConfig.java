d1312d57e75c8e45af449e6b80652bb0ab69ba23
Roshni Kansara <roshnikansara0510@gmail.com>
2026-06-21 19:16:09 +0530

    Allow localhost and Vercel origins

--- backend/src/main/java/taskManagementSystem/security/SecurityConfig.java
+++ backend/src/main/java/taskManagementSystem/security/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
                     new CorsConfiguration();
 
             configuration.setAllowedOrigins(
-                    List.of("http://localhost:5173")
+                    List.of("http://localhost:5173",
+                    "https://task-management-system-ten-cyan.vercel.app")
             );
 
             configuration.setAllowedMethods(
